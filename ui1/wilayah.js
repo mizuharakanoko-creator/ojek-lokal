6b22adc7fb4cd8fdced013b80b00ce047b974e5f
mizuharakanoko-creator <mizuharakanoko@gmail.com>
2026-03-24 21:56:22 +0700

    Update wilayah.js

--- ui1/wilayah.js
+++ ui1/wilayah.js
@@ -1,65 +1,62 @@
-// wilayah.js - Database Wilayah Layanan Masif
+// wilayah.js - Database Berjenjang (Kecamatan -> Desa)
 const dataWilayah = {
-    "DKI Jakarta": [
-        "Jakarta Pusat", "Jakarta Barat", "Jakarta Selatan", "Jakarta Timur", "Jakarta Utara", "Kepulauan Seribu"
-    ],
-    "Bandung Raya": [
-        "Bandung Kota", "Kabupaten Bandung", "Bandung Barat", "Cimahi"
-    ],
-    "Bogor & Depok": [
-        "Bogor Kota", "Kabupaten Bogor", "Depok Kota"
-    ],
-    "Tangerang Raya": [
-        "Tangerang Kota", "Tangerang Selatan", "Kabupaten Tangerang"
-    ],
-    "Bekasi": [
-        "Bekasi Kota", "Kabupaten Bekasi"
-    ],
-    "Priangan Timur": [
-        "Garut", "Tasikmalaya", "Ciamis", "Banjar", "Pangandaran"
-    ],
-    "Cirebon Raya": [
-        "Cirebon Kota", "Kabupaten Cirebon", "Indramayu", "Majalengka", "Kuningan"
-    ],
-    "Jawa Barat Utara": [
-        "Karawang", "Subang", "Purwakarta"
-    ],
-    "Sukabumi & Cianjur": [
-        "Sukabumi Kota", "Kabupaten Sukabumi", "Cianjur"
-    ]
+    "Cigugur": {
+        lat: -6.9712, lng: 108.4554, diff: 1.4, // Koordinat pusat kecamatan
+        desa: [
+            "Cigugur", "Sukamulya", "Cigadung", "Panyuran", 
+            "Cisantana", "Cileuleuy", "Babakanmulya", "Cipari"
+        ]
+    },
+    "Cilimus": {
+        lat: -6.8864, lng: 108.4947, diff: 1.2,
+        desa: [
+            "Cilimus", "Bohtong", "Caracas", "Sampora", 
+            "Kaliaren", "Bandorasa Kulon", "Bandorasa Wetan", "Linggaindah"
+        ]
+    },
+    "Kuningan Kota": {
+        lat: -6.9765, lng: 108.4841, diff: 1.0,
+        desa: [
+            "Kuningan", "Purwawinangun", "Cijoho", "Cirendang", 
+            "Kasturi", "Winduhaji", "Ancaran", "Ciporang"
+        ]
+    },
+    // NANTI LANJUTKAN CICILAN KECAMATAN LAIN DI SINI
 };
 
-// Fungsi inisialisasi Dropdown Step 1
+// --- FUNGSI LOGIKA UPDATE ---
+
 function initWilayah() {
     const kecSelect = document.getElementById('dest-kecamatan');
     if (!kecSelect) return;
-    kecSelect.innerHTML = '<option value="">-- Pilih Wilayah Besar --</option>';
     
-    for (let wilayah in dataWilayah) {
+    kecSelect.innerHTML = '<option value="">-- Pilih Kecamatan --</option>';
+    for (let kecamatan in dataWilayah) {
         let opt = document.createElement('option');
-        opt.value = wilayah;
-        opt.innerHTML = wilayah;
+        opt.value = kecamatan;
+        opt.innerHTML = kecamatan;
         kecSelect.appendChild(opt);
     }
 }
 
-// Fungsi inisialisasi Dropdown Step 2
-function loadCities(wilayah) {
+function loadCities(kecamatan) {
     const citySelect = document.getElementById('dest-city');
     const wrapper = document.getElementById('city-wrapper');
     if (!citySelect || !wrapper) return;
 
-    citySelect.innerHTML = '<option value="">-- Pilih Kota/Kabupaten --</option>';
+    citySelect.innerHTML = '<option value="">-- Pilih Desa/Kelurahan --</option>';
     
-    if (wilayah && dataWilayah[wilayah]) {
+    if (kecamatan && dataWilayah[kecamatan]) {
         wrapper.style.display = 'block';
-        dataWilayah[wilayah].forEach(area => {
+        // Ambil array desa dari kecamatan yang dipilih
+        dataWilayah[kecamatan].desa.forEach((namaDesa) => {
             let opt = document.createElement('option');
-            opt.value = area;
-            opt.innerHTML = area;
+            opt.value = namaDesa; // Value berupa nama desa untuk dikirim ke driver
+            opt.innerHTML = namaDesa;
             citySelect.appendChild(opt);
         });
     } else {
         wrapper.style.display = 'none';
     }
-}
+        }
+                                            
