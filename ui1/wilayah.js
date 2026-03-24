9e493ba8068496a144261655c47cd46e58dff111
mizuharakanoko-creator <mizuharakanoko@gmail.com>
2026-03-24 07:10:45 +0700

    Update wilayah.js

--- ui1/wilayah.js
+++ ui1/wilayah.js
@@ -2,33 +2,18 @@
 const dataWilayah = {
     "Cibeunying": ["Cicadas", "Sukapada", "Padasuka"],
     "Coblong": ["Dago", "Sadang Serang", "Sekeloa"],
-    // Tambahkan ribuan data lainnya di sini nanti
+    "Lengkong": ["Malabar", "Burangrang", "Turangga"]
 };
 
 function initWilayah() {
     const kecSelect = document.getElementById('dest-kecamatan');
+    // Bersihkan dulu
+    kecSelect.innerHTML = '<option value="">-- Pilih Kecamatan --</option>';
+    
     for (let kec in dataWilayah) {
         let opt = document.createElement('option');
         opt.value = kec;
         opt.innerHTML = kec;
         kecSelect.appendChild(opt);
-    }
-}
-
-function loadCities(kec) {
-    const citySelect = document.getElementById('dest-city');
-    const wrapper = document.getElementById('city-wrapper');
-    citySelect.innerHTML = '<option value="">-- Pilih Kelurahan --</option>';
-    
-    if(kec && dataWilayah[kec]) {
-        wrapper.style.display = 'block';
-        dataWilayah[kec].forEach(city => {
-            let opt = document.createElement('option');
-            opt.value = city;
-            opt.innerHTML = city;
-            citySelect.appendChild(opt);
-        });
-    } else {
-        wrapper.style.display = 'none';
     }
 }
