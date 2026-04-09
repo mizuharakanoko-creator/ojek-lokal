4cb278972c785aa676c9d64c2426d3871e5d4dc9
mizuharakanoko-creator <mizuharakanoko@gmail.com>
2026-04-10 03:11:26 +0700

    Create shard-config.js

---
+++ ui7/shard-config.js
@@ -0,0 +1,14 @@
+// Simpan file ini dengan nama: shard-config.js
+const SHARD_MASTER = {
+    "IDN-JABAR-KNG-JALAKSANA": {
+        apiKey: "AIzaSyA8gSce2OvSC0hece_r_kifBKoG8mkVZBk", // Ganti dengan Key Shard Jalaksana nanti
+        databaseURL: "https://ojeklokal-jalaksana-default-rtdb.firebaseio.com",
+        projectId: "ojeklokal-jalaksana"
+    },
+    "IDN-JABAR-KNG-KRAMATMULYA": {
+        apiKey: "AIzaSyA8gSce2OvSC0hece_r_kifBKoG8mkVZBk", // Ganti dengan Key Shard Kramatmulya nanti
+        databaseURL: "https://ojeklokal-kramatmulya-default-rtdb.firebaseio.com",
+        projectId: "ojeklokal-kramatmulya"
+    }
+    // Tambahkan baris baru setiap kali Anda membuat Firebase gratisan baru
+};
