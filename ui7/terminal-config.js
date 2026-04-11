096fd5102f76684dfca533c2a0462ce6c05a6f41
mizuharakanoko-creator <mizuharakanoko@gmail.com>
2026-04-11 09:15:10 +0700

    Create terminal-config.js

---
+++ ui7/terminal-config.js
@@ -0,0 +1,9 @@
+// File: terminal-config.js
+const TERMINAL_MASTER = {
+    "PENGATURAN_PUSAT": {
+        apiKey: "AIzaSyA8gSce2OvSC0hece_r_kifBKoG8mkVZBk",
+        authDomain: "ojeklokal-42b84.firebaseapp.com",
+        databaseURL: "https://ojeklokal-42b84-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "ojeklokal-42b84"
+    }
+};
