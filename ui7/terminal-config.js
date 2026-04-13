9eace9806fb7069997ca51b1a2ed1527bd3d91a6
mizuharakanoko-creator <mizuharakanoko@gmail.com>
2026-04-13 16:31:49 +0700

    Delete ui7 directory

--- ui7/terminal-config.js
+++
@@ -1,9 +0,0 @@
-// Nama variabel: TERMINAL_MASTER
-const TERMINAL_MASTER = {
-    "PENGATURAN_PUSAT": {
-        apiKey: "AIzaSyA8gSce2OvSC0hece_r_kifBKoG8mkVZBk",
-        authDomain: "ojeklokal-42b84.firebaseapp.com",
-        databaseURL: "https://ojeklokal-42b84-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "ojeklokal-42b84"
-    }
-};
